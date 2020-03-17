fb91119dad94e95333211b22f75b295c77a9a15f
Sean-K-Wu <x7430657@163.com>
2020-03-18 00:34:47 +0800

    增加clone测试

---
+++ src/main/java/cn/wuxiangknwo/java7/cloneTest/CloneObject.java
@@ -0,0 +1,6 @@
+package cn.wuxiangknwo.java7.cloneTest;
+
+public class CloneObject implements Cloneable{
+
+    public  int i = 0;
+}
---
+++ src/main/java/cn/wuxiangknwo/java7/cloneTest/CloneObjectTest.java
@@ -0,0 +1,10 @@
+package cn.wuxiangknwo.java7.cloneTest;
+
+public class CloneObjectTest {
+
+    public static void main(String[] args) {
+        CloneObject cloneObject = new CloneObject();
+        cloneObject.i++;
+        cloneObject.clone();
+    }
+}
